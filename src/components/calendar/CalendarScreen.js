72c2f5ccc00d70fe37d4850d7a26c68dfce12ee6
emanuelhardwell <emanuelnoe16@gmail.com>
2021-02-17 23:39:04 -0600

    helper configuration created and styles created

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -3,19 +3,39 @@ import { Navbar } from "../ui/Navbar";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "moment/locale/es";
+import { messages } from "../../helpers/calendar-messages-es";
 
 const localizer = momentLocalizer(moment); // or globalizeLocalizer
+moment.locale("es");
 
 const events = [
   {
     title: "Cumple de Hardwell",
-    start: moment().toDate(),
-    end: moment().add(2, "hours").toDate(),
+    start: moment().subtract(4, "hours").toDate(),
+    end: moment().subtract(2, "hours").toDate(),
     bgcolor: "#fafafa",
+    notes: "Llevar pastel por favor",
   },
 ];
 
 export const CalendarScreen = () => {
+  /*  */
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    /* console.log(event, start, end, isSelected); */
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+
+    return {
+      style,
+    };
+  };
+
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -24,6 +44,8 @@ export const CalendarScreen = () => {
         events={events}
         startAccessor="start"
         endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   );
